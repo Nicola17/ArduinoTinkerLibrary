f80a7a506da38874f5fd3ad9e37f6de8f0f21cdc
Nicola <nicola.pezzotti@gmail.com>
2013-10-25 19:06:14 +0200

    Create AbstractSensor.h

---
+++ AbstractSensor.h
@@ -0,0 +1,15 @@
+#ifndef ABSTRACTSENSOR_H
+#define ABSTRACTSENSOR_H
+
+#include <arduino.h>
+
+namespace Tinker{
+
+  class AbstractSensor{
+  public:
+    virtual float value()=0;
+  };
+
+}
+
+#endif
