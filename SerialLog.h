b6100c61d98a44e2c0c3027d0d4a126e0ea0f34b
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2013-11-21 20:11:28 +0100

    SerialLog revision

--- SerialLog.h
+++ SerialLog.h
@@ -1,8 +1,9 @@
 #ifndef SERIALLOG_H
 #define SERIALLOG_H
 
-namespace Tinker{
+#include "AbstractLog.h"
 
+namespace Tinker{
     //! Arduino serial logger
     /*!
         The SerialLog class is used to send any log on the Arduino Serial port
@@ -11,44 +12,27 @@ namespace Tinker{
     class SerialLog: public AbstractLog{
     public:
         //! Endline.
-        virtual void endline(){
-          Serial.println("");
-        }
+        virtual void endline();
         //! Display a character array.
-        virtual void display(char* str){
-          Serial.print(str);
-        }
+        virtual void display(char* str);
         //! Display a byte value.
-        virtual void display(byte v){
-          Serial.print(v);
-        }
+        virtual void display(byte v);
         //! Display a float value.
-        virtual void display(float v){
-          Serial.print(v);
-        }
+        virtual void display(float v);
         //! Display a double value.
-        virtual void display(double v){
-          Serial.print(v);
-        }
+        virtual void display(double v);
         //! Display an integer value.
-        virtual void display(int v){
-          Serial.print(v);
-        }
+        virtual void display(int v);
         //! Display an unsigned int value.
-        virtual void display(unsigned int v){
-          Serial.print(v);
-        }
+        virtual void display(unsigned int v);
         //! Display an unsigned long value.
-        virtual void display(unsigned long v){
-          Serial.print(v);
-        }
+        virtual void display(unsigned long v);
         //! Clear the logger.
         /*!
             \warning Is not possibile to to clear data on Serial.
         */
-        virtual void clear(){}; 
+        virtual void clear(); 
     };
-
 }
 
 #endif
