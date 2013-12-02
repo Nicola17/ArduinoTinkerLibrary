daa781b156860ced32a4728df4efdfd8511d8c29
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2013-12-02 20:45:27 +0100

    Add pull up resistor handling and "enable on low" to toggle switch

--- ToggleSwitch.h
+++ ToggleSwitch.h
@@ -10,18 +10,32 @@
  ****************************************/
 namespace Tinker{
 
+	//! A class to handle a toggle switch
+	/*!
+		This class permits to handle in an easy way any toggle switch
+		\author Nicola Pezzotti
+	*/
 	class ToggleSwitch{
 	public:
-		ToggleSwitch():_log(0){}
-		ToggleSwitch(int pin);
-		void setPin(int pin);
+		//! Creates an unitialize toggle switch. 
+		ToggleSwitch(uint16_t pin = -1):_log(0),_pin(pin){}
+		//! Returns current switch pin
 		int pin()const;
-		void initialize();
-		
+		//! Sets switch pin
+		void setPin(uint16_t pin);
+		//! Returns true if the switch is enabled on LOW
+		boolean enabledOnLow()const;
+		//! Returns true if pull up resistor is enabled
+		boolean pullUpResistor()const;
+		//! Initialiazes the toggle switch
+		void initialize(bool enabledOnLow = false, bool pullUpResistor = false);
+		//!Returns toggle switch status
 		virtual boolean enabled()const;
 		
 	private:
-		int _pin;
+		uint16_t _pin;
+		bool _enabledOnLow;
+		bool _pullUpResistor;
 
 	public:
 		AbstractLog* _log;
